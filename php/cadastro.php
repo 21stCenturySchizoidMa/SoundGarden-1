b1fb5dfa993e4e5930a92a5dd4bf25895446c1e1
ruan linhares <ruanvitor0941@gmail.com>
2024-07-01 11:33:58 -0300

    ultimate definitive edtion blast master ultra

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,19 +1,18 @@
 <?php
- include("conexao.php");
- 
- $user =$_POST['user'];
- $email =$_POST['email'];
- $senha =md5($_POST['senha']);
+   include("conexao.php");
+   
+   $user =$_POST['user'];
+   $email =$_POST['email'];
+   $senha =md5($_POST['senha']);
 
- $sql = "INSERT INTO cadastroinfo (user, email, senha) 
-            VALUES ('$user', '$email', '$senha')";
+   $sql = "INSERT INTO cadastroinfo (user, email, senha) 
+               VALUES ('$user', '$email', '$senha')";
 
- if(mysqli_query($conn, $sql)){
-    echo "usuario cadastrado com sucesso";
- }
- else{
-    echo "deu errado maninho";
- }
- mysqli_close($conn);
- 
+   if(mysqli_query($conn, $sql)){
+      echo "usuario cadastrado com sucesso";
+   }
+   else{
+      echo "deu errado maninho";
+   }
+   mysqli_close($conn);
 ?>
